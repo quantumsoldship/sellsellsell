29884e2f25905f8ba3d970ea51ad32519429e769
Isaac N <109833693+quantumsoldship@users.noreply.github.com>
2024-07-06 20:06:13 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import time as t
+import game_functions as game
+
+game.begin()
+game.explain_parts()
+
+    
+print('Congrats! You\'ve ordered all your parts needed to make your pet rocks!')
+t.sleep(3)
+print('While you wait for your parts to arrive, let\'s pay back off your first month of your loan to the bank!')
+game.pay_the_bank()
+
+print('Your parts look like they have arrived!')
+print('')
+
