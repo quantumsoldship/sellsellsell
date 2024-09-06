999f15f8aad1357287d6836dfdd98d38ebb36673
leafers58 <109424116+leafers58@users.noreply.github.com>
2024-09-06 11:31:30 -0400

    commited

--- game_functions.py
+++ game_functions.py
@@ -5,7 +5,9 @@ import time as t
 first_manufacture = True
 machinery_level = 0
 factory_level = 0
-
+eyes = 0
+rocks = 0
+paint = 0
 min_credit_score = 1
 
 credit_score = 5
@@ -203,9 +205,22 @@ def expand():
 
 
 def sell():
-    print('To sell your goods, you)
+    print('To sell your goods, you')
 def manufacture():
+    global paint, eyes, rocks
+    print('Manufacturing goods...')
+    print('Amount of rocks: ' + rocks)
+    print('Amount of paint: ' + paint)
+    print('Amount of googly eyes: ' + eyes)
+    sleep(1)
+    print('How many batches would you like to manufacture?')
     print('')
+    for i in range(min(paint,eyes,rocks)):
+        print('Manufacturing goods...')
+        sleep(r.randint(1,3))
+        print('Manufacturing complete, adding to storage.')
+        sleep(1)
+
 def dashboard():
     print('Pay off loan (l)')
     print('Manufacture products (m)')
